b0eb6448f4fe632aaf788664105bb29ee0791529
Justin Pounders <jmpounders@gmail.com>
2024-01-04 16:39:31 -0500

    update

--- src/groundcrew/llm/openaiapi.py
+++ src/groundcrew/llm/openaiapi.py
@@ -6,6 +6,7 @@ from typing import Any, List, Callable, Optional
 import os
 import sys
 from dataclasses import dataclass
+import json
 
 import openai as oai
 import torch
@@ -16,16 +17,11 @@ EMBEDDING_MODEL_DEFAULT = 'text-embedding-ada-002'
 
 
 @dataclass
-class ToolFunction:
-    name: str
-    arguments: str
-
-
-@dataclass
 class ToolCall:
     tool_call_id: str
     tool_type: str
-    function: ToolFunction
+    function_name: str
+    function_args: dict
 
 
 @dataclass
@@ -76,30 +72,12 @@ def load_api_key(file_path: str) -> str:
     return res
 
 
-def get_models_list() -> List:
-    """Return a list of available models."""
-    model_obj = oai.models.list()
-    return [model.id for model in model_obj.data]
-
-
-def embeddings(client: oai.Client, texts: List[str]) -> Any:
+def embeddings(client: oai.Client, texts: list[str]) -> Any:
     """get embeddings, specifying the default model"""
     return client.embeddings.create(
         input=texts,
         model=EMBEDDING_MODEL_DEFAULT
     )
-
-
-def chat_completion(client: oai.Client, prompt: str, model: str) -> str:
-    """simple chat completion"""
-    res = client.chat.completions.create(
-        model=model,
-        messages=[
-            {'role': 'system', 'content': 'You are a helpful assistant.'},
-            {'role': 'user', 'content': prompt}
-        ]
-    )
-    return res['choices'][0]['message']['content']
 
 
 def extract_embeddings(data: Any) -> torch.Tensor:
@@ -110,7 +88,7 @@ def extract_embeddings(data: Any) -> torch.Tensor:
     return torch.stack(embs, dim=0)
 
 
-def embeddings_tensor(client: oai.Client, texts: List[str]) -> torch.Tensor:
+def embeddings_tensor(client: oai.Client, texts: list[str]) -> torch.Tensor:
     """get a tensor of embeddings from a list of strings"""
     embs = embeddings(client, texts)
     res = extract_embeddings(embs)
@@ -118,30 +96,56 @@ def embeddings_tensor(client: oai.Client, texts: List[str]) -> torch.Tensor:
     return res
 
 
+def toolcall_to_dict(tool_call: ToolCall) -> dict:
+    """Convert a ToolCall to a dict."""
+    return {
+        'id': tool_call.tool_call_id,
+        'type': 'function',
+        'function': {
+            'name': tool_call.function_name,
+            'arguments': json.dumps(tool_call.function_args)
+        }
+    }
+
 def message_to_dict(message: Message) -> dict:
-    """Because it's much, much faster than built in dataclasses.asdict."""
-    return {key:value for key,value in vars(message).items() if value is not None}
+    """Convert a message to a dict.
+
+    This is much, much faster than the built in dataclasses.asdict function."""
+
+    output_dict = {}
+    for key,value in vars(message).items():
+        if value is None:
+            continue
+
+        if isinstance(value, list):
+            # There are sometimes lists of messages w/in messages
+            output_dict[key] = [
+                vi if not isinstance(vi, ToolCall) else toolcall_to_dict(vi)
+                for vi in value
+            ]
+        else:
+            output_dict[key] = value
+
+    return output_dict
 
 
 def message_from_api_response(response: dict) -> AssistantMessage:
     """Parse the API response."""
     completion = response.choices[0].message
 
-    role = completion.role
-    assert role=='assistant'
-
-    content = completion.content
-    tool_calls = completion.tool_calls
-    if tool_calls is not None:
+    if completion.tool_calls is not None:
         tool_calls = [
             ToolCall(
-                tool_call['id'],
-                tool_call['type'],
-                ToolFunction(**tool_call['function']))
-            for tool_call in tool_calls
+                tool_call.id,
+                tool_call.type,
+                tool_call.function.name,
+                json.loads(tool_call.function.arguments))
+            for tool_call in completion.tool_calls
         ]
+    else:
+        tool_calls = None
 
-    return AssistantMessage(content, tool_calls)
+    return AssistantMessage(completion.content, tool_calls)
 
 
 def message_from_tool_call(tool_call_id: str, function_output: Any) -> ToolMessage:
@@ -150,22 +154,26 @@ def message_from_tool_call(tool_call_id: str, function_output: Any) -> ToolMessa
 
 
 def start_chat(model: str, api_key: str) -> Callable:
-    """Make an LLM interface function that you can use with Messages."""
+    """Make an LLM interface function that you can use with Messages.
+
+    This will return a function that can be called with a list of messages.
+    Optional arguments to this function should conform with parameter requirements
+    of the OpenAI API, e.g., `tools`, `temperature`, `seed`, etc."""
 
     client = get_openaiai_client(api_key=api_key)
 
-    def chat_func(messages: List[Message], *args, **kwargs) -> Message:
+    def chat_func(messages: list[Message], *args, **kwargs) -> Message:
         input_messages = [message_to_dict(message) for message in messages]
-        try:
-            response = client.chat.completions.create(
-                messages=input_messages,
-                model=model,
-                *args,
-                **kwargs
-            )
-            print('from api', response)
-            return message_from_api_response(response)
-        except oai.APIError:
-            return UserMessage('There was an API error.  Please try again.')
+        print(input_messages)
+        # try:
+        response = client.chat.completions.create(
+            messages=input_messages,
+            model=model,
+            *args,
+            **kwargs
+        )
+        return message_from_api_response(response)
+        # except oai.APIError:
+        #     return UserMessage('There was an API error.  Please try again.')
 
     return chat_func
--- tests/llm/test_openaiapi.py
+++ tests/llm/test_openaiapi.py
@@ -5,104 +5,164 @@ Tests for OpenAI API.
 from unittest.mock import patch
 
 import pytest
+import openai as oai
+import json
 
 from groundcrew.llm import openaiapi
 
 @pytest.fixture
 def openai_chat_response():
     def _chat_output(content, role):
-        return {
-            "choices": [
-                {
-                    "finish_reason": "stop",
-                    "index": 0,
-                    "message": {
-                        "content": content,
-                        "role": role
-                    }
-                }
+        return oai.types.chat.ChatCompletion(
+            choices=[
+                oai.types.chat.chat_completion.Choice(
+                    finish_reason="stop",
+                    index=0,
+                    logprobs=None,
+                    message=oai.types.chat.ChatCompletionMessage(
+                        content=content,
+                        role=role
+                    )
+                )
             ],
-            "created": 1689623190,
-            "id": "chatcmpl-xyz",
-            "model": "gpt-3.5-turbo-0613-mock",
-            "fingerprint": "fp_44709d6fcb",
-            "object": "chat.completion",
-            "usage": {
-                "completion_tokens": 99,
-                "prompt_tokens": 99,
-                "total_tokens": 99
-            }
-        }
+            created=1689623190,
+            id="chatcmpl-xyz",
+            model="gpt-3.5-turbo-0613-mock",
+            system_fingerprint="fp_44709d6fcb",
+            object="chat.completion",
+            usage=oai.types.CompletionUsage(
+                completion_tokens=99,
+                prompt_tokens=99,
+                total_tokens=99
+            )
+        )
 
     return _chat_output
 
 
-@patch('neosophia.llmtools.openaiapi.embeddings')
-def test_embeddings_tensor(embeddings_mock):
-    """test `embeddings_tensor` and `extract_embeddings` functions"""
-    n_texts = 3
+@pytest.fixture
+def openai_tool_response():
+    def _chat_output(content, role, function_name, function_args):
+        return oai.types.chat.ChatCompletion(
+            choices=[
+                oai.types.chat.chat_completion.Choice(
+                    finish_reason='stop',
+                    index=0,
+                    logprobs=None,
+                    message=oai.types.chat.ChatCompletionMessage(
+                        content=content,
+                        role=role,
+                        tool_calls=[
+                            oai.types.chat.chat_completion_message_tool_call.ChatCompletionMessageToolCall(
+                                id='some_string',
+                                function=oai.types.chat.chat_completion_message_tool_call.Function(
+                                    arguments=function_args,
+                                    name=function_name
+                                ),
+                                type='function'
+                            )
+                        ]
+                    )
+                )
+            ],
+            created=1689623190,
+            id="chatcmpl-xyz",
+            model="gpt-3.5-turbo-0613-mock",
+            system_fingerprint="fp_44709d6fcb",
+            object="chat.completion",
+            usage=oai.types.CompletionUsage(
+                completion_tokens=99,
+                prompt_tokens=99,
+                total_tokens=99
+            )
+        )
 
-    # OpenAI API returns a data struction which contains an embedding
-    # for each input
-    example_data = {
-        'data': [
-            {
-                'embedding': [0.0] * openaiapi.EMBEDDING_DIM_DEFAULT
-            }
-        ] * n_texts
-
-    }
-    embeddings_mock.return_value = example_data
-    texts = ['baloney'] * n_texts
-
-    res = openaiapi.embeddings_tensor(texts)
-    assert res.shape == (n_texts, openaiapi.EMBEDDING_DIM_DEFAULT)
+    return _chat_output
 
 
-def test_messages(openai_chat_response):
-    message = openaiapi.Message('system', 'You are a helpful assistant.')
-    assert message.is_valid()
-    assert message.as_dict()=={
-        'role': 'system',
-        'content': 'You are a helpful assistant.'
-    }
+# @patch('neosophia.llmtools.openaiapi.embeddings')
+# def test_embeddings_tensor(embeddings_mock):
+#     """test `embeddings_tensor` and `extract_embeddings` functions"""
+#     n_texts = 3
 
-    target_output_content = 'This is output from the LLM.'
-    target_output_role = 'assistant'
-    response = openai_chat_response(
-        target_output_content,
-        target_output_role
+#     # OpenAI API returns a data struction which contains an embedding
+#     # for each input
+#     example_data = {
+#         'data': [
+#             {
+#                 'embedding': [0.0] * openaiapi.EMBEDDING_DIM_DEFAULT
+#             }
+#         ] * n_texts
+
+#     }
+#     embeddings_mock.return_value = example_data
+#     texts = ['baloney'] * n_texts
+
+#     res = openaiapi.embeddings_tensor(texts)
+#     assert res.shape == (n_texts, openaiapi.EMBEDDING_DIM_DEFAULT)
+
+
+def test_message_to_dict():
+    message = openaiapi.UserMessage('This is a test.')
+    message_dict = openaiapi.message_to_dict(message)
+    assert message_dict['role']=='user'
+    assert message_dict['content']=='This is a test.'
+
+    message = openaiapi.AssistantMessage('This is a test.', None)
+    message_dict = openaiapi.message_to_dict(message)
+    assert message_dict['role']=='assistant'
+    assert message_dict['content']=='This is a test.'
+    assert 'tool_calls' not in message_dict
+
+    message = openaiapi.AssistantMessage(
+        'This is a test.',
+        [openaiapi.ToolCall('tcid', 'function', 'func_name', {'arg1': 42})]
     )
-    message = openaiapi.Message.from_api_response(response)
-    assert message.is_valid()
-    assert message==openaiapi.Message(
-        role=target_output_role,
-        content=target_output_content,
-        name=None
-    )
+    message_dict = openaiapi.message_to_dict(message)
+    assert message_dict['role']=='assistant'
+    assert message_dict['content']=='This is a test.'
+    assert message_dict['tool_calls']==[{
+        'id': 'tcid',
+        'type': 'function',
+        'function': {
+            'name': 'func_name',
+            'arguments': '{"arg1": 42}'
+        }
+    }]
 
-    message = openaiapi.Message.from_function_call('get_weather', 72)
-    assert message.is_valid()
-    assert message==openaiapi.Message(
-        role='function',
-        content='72',
-        name='get_weather'
-    )
-
-
-@patch('neosophia.llmtools.openaiapi.oai.ChatCompletion.create')
-def test_chat_completion(chat_mock, openai_chat_response):
+@patch('groundcrew.llm.openaiapi.oai.resources.chat.completions.Completions.create')
+def test_chat_completion(chat_mock, openai_chat_response, openai_tool_response):
     target_output_content = 'Who is there?'
     target_output_role = 'assistant'
     chat_mock.return_value = openai_chat_response(
         target_output_content,
         target_output_role
     )
-    model = openaiapi.start_chat('test_model')
+    model = openaiapi.start_chat('test_model', 'fake_api_key')
     messages = [
-        openaiapi.Message('system', 'You are a helpful assistant.'),
-        openaiapi.Message('user', 'Knock knock.')
+        openaiapi.SystemMessage('You are a helpful assistant.'),
+        openaiapi.UserMessage('Knock knock.')
     ]
     response = model(messages)
     assert response.content==target_output_content
     assert response.role==target_output_role
+
+
+    target_output_function_name = 'test_func'
+    target_output_function_args = '{"arg1": 42}'
+    chat_mock.return_value = openai_tool_response(
+        target_output_content,
+        target_output_role,
+        target_output_function_name,
+        target_output_function_args
+    )
+    model = openaiapi.start_chat('test_model', 'fake_api_key')
+    messages = [
+        openaiapi.SystemMessage('You are a helpful assistant.'),
+        openaiapi.UserMessage('Knock knock.')
+    ]
+    response = model(messages)
+    assert response.content==target_output_content
+    assert response.role==target_output_role
+    assert response.tool_calls[0].function_name==target_output_function_name
+    assert response.tool_calls[0].function_args==json.loads(target_output_function_args)
