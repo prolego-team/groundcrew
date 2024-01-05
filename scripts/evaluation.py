3647d0e9c03142abe2a973c664f0d153b7dbde65
Ben Zimmer <ben.d.zimmer@gmail.com>
2024-01-05 15:06:43 -0600

    update

---
+++ scripts/evaluation.py
@@ -0,0 +1,17 @@
+"""
+Evaluation framework.
+"""
+
+# Copyright (c) 2023 Prolego Inc. All rights reserved.
+# Ben Zimmer
+
+from typing import Any, Dict, List, Optional, Tuple
+import os
+
+
+def main():
+    """main program"""
+
+
+if __name__ == '__main__':
+    main()
