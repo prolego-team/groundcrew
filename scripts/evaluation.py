e21b6d4616f240b63f9252c12d815c1b0a436af9
Ben Zimmer <ben.d.zimmer@gmail.com>
2024-01-08 18:57:57 -0600

    update

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -5,9 +5,11 @@ Evaluation framework.
 # Copyright (c) 2023 Prolego Inc. All rights reserved.
 # Ben Zimmer
 
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Dict, List, Callable
+import datetime
 import os
 import re
+import time
 
 import chromadb
 import click
@@ -25,8 +27,18 @@ from scripts import run
 @click.option('--config', '-c', default='config.yaml')
 @click.option('--model', '-m', default='gpt-3.5-turbo')
 @click.option('--evals', '-e', default='data/evaluation.yaml')
-def main(config, model, evals):
+@click.option('--n_runs', '-n', default=3)
+@click.option('--output_dir_prefix', '-o', default='eval')
+def main(config, model, evals, n_runs, output_dir_prefix):
     """main program"""
+
+    # ~~~~ create output directory
+
+    timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+    output_dir_path = f'{output_dir_prefix}_{timestamp}'
+    os.makedirs(output_dir_path, exist_ok=False)
+
+    # ~~~~ build the system (collection, llm, etc)
 
     # For now we'll assume that `run.py` has been run
     # and the database is present
@@ -49,7 +61,9 @@ def main(config, model, evals):
         embedding_function=code.DEFAULT_EF
     )
 
-    llm = run.build_llm_client(model)
+    # OpenAI models can't be created with a seed
+    # so this is a simple wrapper that ignores the seed
+    llm_from_seed = lambda _: run.build_llm_client(model)
 
     tools_dict = dict(
         codebase_qa=tools.codebase_qa
@@ -59,38 +73,116 @@ def main(config, model, evals):
         match_word_any=match_word_any
     )
 
-    # Run evaluations
+    # ~~~~ run evaluations
+
+    results = {}
+
+    # TODO: runs across multiple systems
 
     for suite in evals:
-        run_suite(
-            suite,
-            eval_funcs_dict,
-            collection,
-            llm,
-            tools_dict,
+        results_suite = run_suite(
+            suite=suite,
+            n_runs=n_runs,
+            eval_funcs_dict=eval_funcs_dict,
+            collection=collection,
+            llm_from_seed=llm_from_seed,
+            tools_dict=tools_dict,
         )
+        results = {**results, **results_suite}
 
-        # TODO: proper returns
+    output_file_prefix = 'eval'
+    output_file_path = os.path.join(output_dir_path, f'{output_file_prefix}.csv')
+    # header = 'suite,question,run,calls,time,missing,correct,answer\n'
+    header = 'suite,question,run,time,missing,correct,answer\n'
+    with open(output_file_path, 'w') as f:
+        f.write(header)
+        for (suite_name, question, run_idx), info in results.items():
+            answer = info['answer']
+            if answer is not None:
+                answer = answer.replace('\n', '%%%').replace('"', '``')
+            line = [
+                f'"{suite_name}"',
+                f'"{question}"',
+                run_idx,
+                # info['calls'],
+                info['time'],
+                info['missing'],
+                info['correct'],
+                f'"{answer}"'
+            ]
+            line = [str(x) for x in line]
+            line = ','.join(line) + '\n'
+            f.write(line)
+
+    print(f'wrote `{output_file_path}`')
+
+    # TODO: when we add the capability to run on different "systems"
+    #       we can add functionality to aggregate across system
+    #       and system + question
 
 
-def run_suite(suite: Dict, eval_funcs_dict, collection, llm, tools_dict) -> None:
-    """run a test suite and print the results"""
-    print(suite)
-    print(suite['name'])
-    res = []
+def run_suite(
+        suite: Dict,
+        n_runs: int,
+        eval_funcs_dict: Dict,
+        collection: chromadb.Collection,
+        llm_from_seed: Callable,
+        tools_dict: Dict
+        ) -> Dict:
+    """run a test suite and return results"""
+
+    results = {}
+
+    suite_name = suite['name']
+
+    print(suite_name)
+
+    success = []
+
     for test in tqdm.tqdm(suite['tests']):
-        tool = tools_dict[test['tool']]
-        question = test['question']
-        eval_params = dict(test['eval_func'])
-        eval_func = eval_funcs_dict[eval_params['typ']]
-        del eval_params['typ']
+        for run_idx in range(n_runs):
+            tool = tools_dict[test['tool']]
+            question = test['question']
+            eval_params = dict(test['eval_func'])
+            eval_func = eval_funcs_dict[eval_params['typ']]
+            del eval_params['typ']
 
-        answer = tool(question, collection, llm)
-        res_test = eval_func(answer, **eval_params)
-        res.append(res_test)
+            llm = llm_from_seed(run_idx)
 
-    print(sum(res), '/', len(res), 'passed')
+            start_time = time.time()
+
+            try:
+                answer = tool(question, collection, llm)
+            except Exception as e:
+                answer = None
+
+            end_time = time.time()
+            total_time = end_time - start_time
+
+            # TODO: would be nice to have LLM call count here
+
+            info = {
+                'time': round(total_time, 3),
+                'answer': answer,
+            }
+
+            if answer is not None:
+                res_test = eval_func(answer, **eval_params)
+                info['missing'] = False
+                info['correct'] = res_test
+                success.append(res_test)
+            else:
+                info['missing'] = True
+                info['correct'] = False
+                success.append(False)
+
+            uid = (suite_name, question, run_idx)
+            results[uid] = info
+
+    print(sum(success), '/', len(success), 'passed')
     print('~~~~ ~~~~ ~~~~ ~~~~')
+
+    return results
 
 
 def match_word_any(text: str, words: List[str], strip_quotes: bool) -> bool:
