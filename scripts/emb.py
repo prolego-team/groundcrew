99231444c710f163c0cf2418c054aaa0c8916d56
Ben Zimmer <ben.d.zimmer@gmail.com>
2024-01-08 20:05:48 -0600

    update

--- scripts/emb.py
+++ scripts/emb.py
@@ -5,12 +5,52 @@ Figure out how chromadb embedding stuff works.
 # Copyright (c) 2023 Prolego Inc. All rights reserved.
 # Ben Zimmer
 
-from typing import Any, Dict, List, Optional, Tuple
-import os
+
+import chromadb
+import numpy as np
+
+from groundcrew import code
 
 
 def main():
     """main program"""
+
+    # can replace with another text document
+    with open('mtgrules.txt', 'r') as f:
+        rules = f.readlines()
+    rules = ''.join(rules)
+
+    client = chromadb.PersistentClient('baloney')
+
+    collection = client.get_or_create_collection(
+        name=code.DEFAULT_COLLECTION_NAME,
+        embedding_function=code.DEFAULT_EF
+    )
+
+    # https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
+    # "By default, input text longer than 256 word pieces is truncated."
+
+    docs = [
+        rules[:128],
+        rules[:256],
+        rules[:512],
+        rules[:1024],
+        rules[:2048],
+        rules[:4096],
+        rules[:8192],
+    ]
+
+    collection.upsert(
+        documents=docs,
+        ids=[str(x) for x in range(len(docs))]
+    )
+
+    res = collection.get(include=['documents', 'embeddings'])
+
+    # all documents 2k or larger appear to have the same embeddings
+
+    for uid, doc, emb in zip(res['ids'], res['documents'], res['embeddings']):
+        print(uid, len(doc), np.array(emb[:5]), '...', np.array(emb[-5:]))
 
 
 if __name__ == '__main__':
