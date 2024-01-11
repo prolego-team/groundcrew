105c497b66985107500bbb70aa26e7695e73e9fa
Cameron Fabbri <cameronfabbri@gmail.com>
2024-01-11 13:15:17 -0700

    adding colors

--- src/groundcrew/dataclasses.py
+++ src/groundcrew/dataclasses.py
@@ -69,3 +69,15 @@ class Tool:
         output += f'Base Prompt: {self.base_prompt}\n'
         return output
 
+
+class Colors:
+    """ Colors for printing """
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[94m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    ENDC = '\033[0m'
