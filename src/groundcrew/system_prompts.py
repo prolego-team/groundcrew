9cd7cb398a32dfcd91b74952352592ed0713bce6
Cameron Fabbri <cameronfabbri@gmail.com>
2024-01-18 11:52:29 -0700

    updated prompt

--- src/groundcrew/system_prompts.py
+++ src/groundcrew/system_prompts.py
@@ -26,13 +26,13 @@ Parameter_1: Parameter_1 Name | Variable_1 value | parameter type
 Parameter_N: Parameter_N Name | Variable_N value | parameter type
 """
 
-TOOL_GPT_PROMPT = """Your task is to take as input a Python `Tool` class and create a description of the `__call__` method in YAML format like the example below. All `Tools` will include a `prompt` parameter in the `__call__` method.
+TOOL_GPT_PROMPT = """Your task is to take as input a Python `Tool` class and create a description of the `__call__` method in YAML format like the example below. All `Tools` will include a `user_prompt` parameter in the `__call__` method.
 
 Instructions:
 - Your output should be formatted exactly like the example
 - Your output should be in correct YAML format.
 - The `base_prompt` you generate should instruct the Tool on what its task is
-- `prompt` should be excluded when generating the description.
+- `user_prompt` should be excluded when generating the description.
 
 Restrictions:
 - Do not include ```yaml in your answer
@@ -50,17 +50,17 @@ class ToolExample(Tool):
         """ """
         super().__init__(base_prompt, collection, llm)
 
-    def __call__(self, prompt: str, parameter_1: str):
+    def __call__(self, user_prompt: str, parameter_1: str):
 
         # Logic here with parameter_1
         output = ... # output from database from parameter_1
 
-        full_prompt = output + '\n' + self.base_prompt + prompt
+        full_prompt = output + '\n' + self.base_prompt + user_prompt
         return self.llm(full_prompt)
 
 ### Example Output ###
   - name: ToolExample
-    description: This tool takes a prompt and parameter_1, does some logic with parameter_1, then uses a large language model to answer the user's question.
+    description: This tool takes a user_prompt and parameter_1, does some logic with parameter_1, then uses a large language model to answer the user's question.
     base_prompt: Your task is to answer the question given the following data. Be descriptive in your answer.
     params:
       parameter_1:
