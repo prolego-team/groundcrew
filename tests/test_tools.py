6121f347a8247b8468df2c688951aafdc74df869
Cameron Fabbri <cameronfabbri@gmail.com>
2024-01-23 15:09:44 -0700

    unit test

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -4,8 +4,6 @@ Tests for tools.
 import chromadb
 
 from groundcrew import constants, tools
-from groundcrew.code import init_db
-from groundcrew.dataclasses import Config
 
 
 def test_singledocstringtool():
@@ -20,13 +18,15 @@ def test_singledocstringtool():
     collection = client.get_or_create_collection(
         name=constants.DEFAULT_COLLECTION_NAME
     )
-
-    example_ids = ['apples.py', 'bananas.py', 'oranges.py', 'apples.py::foo (function)']
+    example_ids = [
+        'apples.py',
+        'apples.py::foo (function)',
+        'bananas.py',
+    ]
     example_metadatas = [
-        dict(filepath=f'src/{name}.py', text=f'{name} function code')
+        dict(filepath=f'src/{name}.py', text=f'{name} code')
         for name in example_ids
     ]
-
     collection.upsert(
         ids=example_ids,
         metadatas=example_metadatas,
@@ -36,6 +36,8 @@ def test_singledocstringtool():
     all_ids = collection.get()['ids']
 
     tool = tools.SingleDocstringTool('You are a tool.', collection, llm_mock)
+
+    #### Test _determine_context ####
 
     # All are none - model didn't pass anything in
     context = tool._determine_context(
@@ -59,7 +61,12 @@ def test_singledocstringtool():
         code='none', filename='functions.py', function_name='foo')
     assert context == 'file-function'
 
-    # Model passed in a filename and no function name
+    #### End test _determine_context ####
+
+    #### Test _get_function_code ####
+
+    # Model passed in a filename and no function name - return all code from
+    # the matched file
     function_code = tool._get_function_code(
         all_ids=all_ids,
         filename='apples.py',
@@ -67,9 +74,10 @@ def test_singledocstringtool():
         code='none',
         context='file'
     )
-    expected_function_code = 'apples.py function code\n\napples.py::foo (function) function code\n'
+    expected_function_code = 'apples.py code\n\napples.py::foo (function) code\n'
     assert function_code == expected_function_code
 
+    # Model only passed in code - generate docstring for just that code
     code = 'def foo():\n    """\n    This is a docstring.\n    """\n    pass'
     function_code = tool._get_function_code(
         all_ids=all_ids,
@@ -80,6 +88,8 @@ def test_singledocstringtool():
     )
     assert function_code == code
 
+    # Model passed in a function name - generate docstring for just the matched
+    # function
     function_code = tool._get_function_code(
         all_ids=all_ids,
         filename='none',
@@ -87,8 +97,80 @@ def test_singledocstringtool():
         code='none',
         context='function'
     )
-    expected_function_code = 'apples.py::foo (function) function code\n'
+    expected_function_code = 'apples.py::foo (function) code\n'
     assert function_code == expected_function_code
+
+    # Model passed in a filename and a function name - generate docstring for
+    # that specific function in that file
+    function_code = tool._get_function_code(
+        all_ids=all_ids,
+        filename='apples.py',
+        function_name='foo',
+        code='none',
+        context='function'
+    )
+    expected_function_code = 'apples.py::foo (function) code\n'
+    assert function_code == expected_function_code
+
+    #### End test _get_function_code ####
+
+    #### Test _id_matches_file ####
+
+    out = tool._id_matches_file(
+        id_='apples.py',
+        filename='apples.py',
+        function_name='none')
+    assert out is True
+
+    out = tool._id_matches_file(
+        id_='apples.py',
+        filename='apples.py',
+        function_name='foo')
+    assert out is False
+
+    out = tool._id_matches_file(
+        id_='apples.py::foo (function)',
+        filename='apples.py',
+        function_name='foo')
+    assert out is True
+
+    out = tool._id_matches_file(
+        id_='apples.py::foo (function)',
+        filename='apples.py',
+        function_name='none')
+    assert out is True
+
+    out = tool._id_matches_file(
+        id_='apples.py::foo (function)',
+        filename='bananas.py',
+        function_name='none')
+    assert out is False
+
+    #### End test _id_matches_file ####
+
+    #### Test _id_matches_function ####
+
+    out = tool._id_matches_function(
+        id_='apples.py::foo (function)',
+        function_name='foo')
+    assert out is True
+
+    out = tool._id_matches_function(
+        id_='apples.py::foo (function)',
+        function_name='none')
+    assert out is False
+
+    out = tool._id_matches_function(
+        id_='apples.py',
+        function_name='foo')
+    assert out is False
+
+    out = tool._id_matches_function(
+        id_='apples.py',
+        function_name='none')
+    assert out is False
+
+    #### End test _id_matches_function ####
 
 
 def test_lintfiletool():
